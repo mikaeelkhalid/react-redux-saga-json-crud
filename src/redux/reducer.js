5f69f5dc6abd8fb456f0ce38680b388014d1752a
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 01:40:27 +0500

    users reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,30 @@
+import * as types from './actionTypes';
+
 const initialState = {
   isLoading: false,
   users: [],
+  error: null,
 };
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.LOAD_USER_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case types.LOAD_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        users: action.payload,
+      };
+    case types.LOAD_USER_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error,
+      };
     default:
       return state;
   }
