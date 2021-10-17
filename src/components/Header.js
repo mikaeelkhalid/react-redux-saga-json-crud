a76f215688080b597baf0b799efce5ef3eacd26b
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 00:28:57 +0500

    adding navbar

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarItem,
+  MDBNavbarNav,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavbarLink,
+  MDBIcon,
+} from 'mdb-react-ui-kit';
+
+const Header = () => {
+  const [showNavColor, setShowNavColor] = useState(false);
+  const [showNavColorSecond, setShowNavColorSecond] = useState(false);
+  const [showNavColorThird, setShowNavColorThird] = useState(false);
+  return (
+    <MDBNavbar expand='lg' dark bgColor='dark'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand>Contacts</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarColor02'
+          aria-controls='navbarColor02'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavColorSecond(!showNavColorSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse show={showNavColorSecond} navbar id='navbarColor02'>
+          <MDBNavbarNav className='me-auto mb-2 mb-lg-0'>
+            <MDBNavbarItem className='active'>
+              <MDBNavbarLink aria-current='page' href='/'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='/add-edit'>Add User</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='/about'>About</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+};
+
+export default Header;
