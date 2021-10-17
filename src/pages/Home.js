07f9fb53cd65a350b14cb8cd5c525f3f8656acaa
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 01:40:07 +0500

    dispatch users

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUserStart } from '../redux/actions';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUserStart());
+  }, []);
+
   return <div>home</div>;
 };
 
