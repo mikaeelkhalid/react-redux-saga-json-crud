b5c69c14e87a2eda7a817d0c58e407adc650dd6d
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 00:30:46 +0500

    creating user reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  isLoading: false,
+  users: [],
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
