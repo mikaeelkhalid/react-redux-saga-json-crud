f63a38d08a9a12aee9c0edad7e43096a2ec4a3ca
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 12:49:03 +0500

    create user actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,3 +13,17 @@ export const loadUserFailure = (error) => ({
   type: types.LOAD_USER_FAILURE,
   payload: error,
 });
+
+export const createUserStart = () => ({
+  type: types.CREATE_USER_START,
+});
+
+export const createUserSuccess = (user) => ({
+  type: types.CREATE_USER_SUCCESS,
+  payload: user,
+});
+
+export const createUserFailure = (error) => ({
+  type: types.CREATE_USER_FAILURE,
+  payload: error,
+});
