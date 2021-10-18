70c495bf3807b55697926f4f9026450089d80ee6
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 15:34:40 +0500

    handle delete and adding loading spinner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,33 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { loadUserStart } from '../redux/actions';
+import { deleteUserStart, loadUserStart } from '../redux/actions';
+
+import { MDBSpinner } from 'mdb-react-ui-kit';
 
 import DataTable from '../components/DataTable';
+import { toast } from 'react-toastify';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { users } = useSelector((state) => state.users);
+  const { users, isLoading } = useSelector((state) => state.users);
 
   useEffect(() => {
     dispatch(loadUserStart());
-  }, []);
+  }, [dispatch]);
 
+  if (isLoading) {
+    return (
+      <MDBSpinner style={{ marginTop: '200px' }} role='status'>
+        <span className='visually-hidden'>Loading..</span>
+      </MDBSpinner>
+    );
+  }
   const handleDelete = (id) => {
-    console.log('clicked', id);
+    if (window.confirm('Are you sure you want to delete this user?')) {
+      dispatch(deleteUserStart(id));
+      toast.success('User deleted successfully');
+    }
   };
 
   return <DataTable users={users} handleDelete={handleDelete} />;
