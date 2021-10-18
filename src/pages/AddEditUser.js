d74d048cfc838cc08b68549d5b39d9d0c8d9dfcb
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 12:46:04 +0500

    add user form

--- src/pages/AddEditUser.js
+++ src/pages/AddEditUser.js
@@ -1,7 +1,113 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MDBValidation, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
+import { useHistory } from 'react-router';
+
+const initialState = {
+  name: '',
+  email: '',
+  phone: '',
+  address: '',
+};
 
 const AddEditUser = () => {
-  return <div>add edit</div>;
+  const [user, setUser] = useState(initialState);
+  const { name, email, phone, address } = user;
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  return (
+    <MDBValidation
+      className='row g-3'
+      style={{ marginTop: '70px' }}
+      onSubmit={handleSubmit}
+      noValidate
+    >
+      <p className='fs-2 fw-bold'>Add user detail</p>
+      <div
+        style={{
+          margin: 'auto',
+          padding: '20px',
+          maxWidth: '400px',
+          alignContent: 'center',
+        }}
+        className='col-md-6'
+      >
+        <MDBInput
+          label='Name'
+          name='name'
+          required
+          type='text'
+          invalid
+          error='wrong'
+          success='right'
+          value={name}
+          onChange={onInputChange}
+          validation='name required'
+        />
+        <br />
+        <MDBInput
+          label='Email'
+          name='email'
+          required
+          type='email'
+          invalid
+          error='wrong'
+          success='right'
+          value={email}
+          onChange={onInputChange}
+          validation='email required'
+        />
+        <br />
+        <MDBInput
+          label='Phone'
+          name='phone'
+          required
+          type='number'
+          invalid
+          error='wrong'
+          success='right'
+          value={phone}
+          onChange={onInputChange}
+          validation='phone required'
+        />
+        <br />
+        <MDBInput
+          label='Address'
+          name='address'
+          type='text'
+          invalid
+          required
+          error='wrong'
+          success='right'
+          value={address}
+          onChange={onInputChange}
+          validation='address required'
+        />
+        <br />
+        <div className='col-12'>
+          <MDBBtn style={{ marginRight: '10px' }} color='primary' type='submit'>
+            Add
+          </MDBBtn>
+          <MDBBtn
+            onClick={() => history.push('/')}
+            color='danger'
+            type='submit'
+          >
+            Go Back
+          </MDBBtn>
+        </div>
+      </div>
+    </MDBValidation>
+  );
 };
 
 export default AddEditUser;
