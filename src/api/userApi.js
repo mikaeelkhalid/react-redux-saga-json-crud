cf5d4e008093b8bcc7f429d27da65dc412e21a44
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 12:51:52 +0500

    create user api

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const loadUsersApi = async () => {
   return await axios.get('http://localhost:5000/users');
 };
+
+export const createUsersApi = async (user) => {
+  return await axios.post('http://localhost:5000/users', user);
+};
