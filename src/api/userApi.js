e1830c5e7851dd32b7e70dbe7e4e92122c433f0f
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-18 12:57:51 +0500

    typo

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,6 +4,6 @@ export const loadUsersApi = async () => {
   return await axios.get('http://localhost:5000/users');
 };
 
-export const createUsersApi = async (user) => {
+export const createUserApi = async (user) => {
   return await axios.post('http://localhost:5000/users', user);
 };
